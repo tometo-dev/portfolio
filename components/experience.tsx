d1ce36438be9c67e63f9022ce565f5a03c879bfc
Sudhanshu <perfectsudh@gmail.com>
2023-03-17 08:43:40 +0530

    add points to the experience section

--- components/experience.tsx
+++ components/experience.tsx
@@ -16,36 +16,53 @@ import { SectionWrapper } from "./motion/section-wrapper";
 
 const experiences = [
   {
-    title: "Software Developer",
-    company_name: "Aptus Data Labs",
-    icon: aptus,
-    iconBg: "#ffffff",
-    date: "July 2019 - August 2021",
-    points: ["TBD"],
+    title: "Senior Software Engineer",
+    companyName: "Fletch",
+    icon: fletch,
+    iconBg: "#151030",
+    date: "Jan 2023 - Present",
+    points: ["Added features to the existing app", "Maintenence  and improvement of the existing codebase"],
+    techUsed: ["React", "TypeScript", "Redux"],
   },
   {
-    title: "Software Engineer, Frontend",
-    company_name: "Paytm",
-    icon: paytm,
-    iconBg: "#FBEAFF",
-    date: "August 2021 - November 2021",
-    points: ["TBD"],
-  },
-  {
-    title: "Full Stack Developer",
-    company_name: "Interview Kickstart",
+    title: "Software Engineer",
+    companyName: "Interview Kickstart",
     icon: ik,
     iconBg: "#ffffff",
     date: "November 2021 - November 2022",
-    points: ["TBD"],
+    points: [
+      "Integrate payment gateways to faciliate payment in India",
+      "Led the frontend team for the internalization (i18n) of the app",
+      "Added developer tooling around i18n, testing and component stories",
+      "Built framework for integrating any 3rd party analytics tool to the app",
+      "Setup monorepo for the management and release of internal npm packages",
+      "Mentored fellow developers around app architecture and better code practices",
+      "Added minor features to the backend django codebase",
+    ],
+    techUsed: ["React", "TypeScript", "ReactQuery", "NextJS", "TailwindCSS", "TestingLibrary", "Storybook", "Django"],
   },
   {
-    title: "Senior Software Engineer",
-    company_name: "Fletch",
-    icon: fletch,
-    iconBg: "#B39CD0",
-    date: "Jan 2023 - Present",
-    points: ["TBD"],
+    title: "Software Engineer, Frontend",
+    companyName: "Paytm",
+    icon: paytm,
+    iconBg: "#ffffff",
+    date: "August 2021 - November 2021",
+    points: ["Added features to Paytm Seller Panel", "Bug fixes and minor modifications to the PaytmMall app"],
+    techUsed: ["React", "JavaScript"],
+  },
+  {
+    title: "Software Developer",
+    companyName: "Aptus Data Labs",
+    icon: aptus,
+    iconBg: "#ffffff",
+    date: "July 2019 - August 2021",
+    points: [
+      "Built UI for Intelligent Supply Chain Planning Platform using React, TypeScript and Material UI",
+      "Implemented Role-based-access-control for the application",
+      "Managed deployments of the frontend applications using Docker and Github Actions",
+      "Built rest apis using Django and Golang",
+    ],
+    techUsed: ["React", "TypeScript", "TailwindCSS", "ReactQuery", "TestingLibrary", "Redux", "Django", "Golang"],
   },
 ];
 
@@ -63,24 +80,35 @@ function ExperienceCard({ experience }: { experience: Experiences[number] }) {
       iconStyle={{ background: experience.iconBg }}
       icon={
         <div className="flex h-full w-full items-center justify-center">
-          <Image src={experience.icon} alt={experience.company_name} className="h-[60%] w-[60%] object-contain" />
+          <Image src={experience.icon} alt={experience.companyName} className="h-[60%] w-[60%] object-contain" />
         </div>
       }
     >
       <div>
         <h3 className="text-[24px] font-bold text-white">{experience.title}</h3>
         <p className="text-[16px] font-semibold text-secondary" style={{ margin: 0 }}>
-          {experience.company_name}
+          {experience.companyName}
         </p>
       </div>
 
       <ul className="mt-5 ml-5 list-disc space-y-2">
         {experience.points.map((point, index) => (
-          <li key={`experience-point-${index}`} className="pl-1 text-[14px] tracking-wider text-white-100">
+          <li key={`experience-point-${index}`} className="pl-1 text-sm tracking-wider text-white-100">
             {point}
           </li>
         ))}
       </ul>
+
+      <div className="mt-4 text-sm tracking-wider text-white-100">
+        <div>Tech used:</div>
+        <div className="flex flex-wrap">
+          {experience.techUsed.map((tech, index) => (
+            <span key={`experience-tech-${index}`} className="mr-2">
+              {tech}
+            </span>
+          ))}
+        </div>
+      </div>
     </VerticalTimelineElement>
   );
 }
