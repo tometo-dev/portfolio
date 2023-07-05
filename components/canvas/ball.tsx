7d076af2130c96b96c5ce689202fe07e5133181b
Sudhanshu <perfectsudh@gmail.com>
2023-07-05 10:00:29 +0530

    chore: bump deps

--- components/canvas/ball.tsx
+++ components/canvas/ball.tsx
@@ -53,7 +53,7 @@ function Ball(props: { imgUrl: string; link: string; name: string }) {
       >
         <icosahedronGeometry args={[1, 1]} />
         <meshStandardMaterial color="#fff8eb" polygonOffset polygonOffsetFactor={-5} flatShading />
-        <Decal position={[0, 0, 1]} rotation={[2 * Math.PI, 0, 6.25]} scale={1} map={decal} flatShading />
+        <Decal position={[0, 0, 1]} rotation={[2 * Math.PI, 0, 6.25]} scale={1} map={decal} />
       </mesh>
     </Float>
   );
